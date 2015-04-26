98bf188dfd1bb36bba0a6e45497b7d4367a815e9
roris <roris@users.noreply.github.com>
2015-04-26 20:27:17 +0500

    Sync with working tree
    Bug fixes and enhancements.
    
    Close the current file when opening a new one. The close function bring
    prompts the user to save if any changes were made.
    
    Added a double click handler. Double clicking on a label now brings up
    the color dialog.
    
    Fixed the color getting rendered wrong. Apparently, .color files are in
    BGRA format instead of RBGA. This lead to some weird colors showing up
    in game.
    
    Add function now adds after the selected label. Del deletes the selected
    label.
    
    Added a text label for changing alpha value.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -31,7 +31,6 @@
 			this.colorDialog = new System.Windows.Forms.ColorDialog();
 			this.menuStrip1 = new System.Windows.Forms.MenuStrip();
 			this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-			this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
 			this.openToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
 			this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
 			this.saveAsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
@@ -42,6 +41,8 @@
 			this.button1 = new System.Windows.Forms.Button();
 			this.button2 = new System.Windows.Forms.Button();
 			this.button3 = new System.Windows.Forms.Button();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
 			this.menuStrip1.SuspendLayout();
 			this.SuspendLayout();
 			// 
@@ -65,7 +66,6 @@
 			// fileToolStripMenuItem
 			// 
 			this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.openToolStripMenuItem,
             this.openToolStripMenuItem1,
             this.saveToolStripMenuItem,
             this.saveAsToolStripMenuItem,
@@ -74,18 +74,11 @@
 			this.fileToolStripMenuItem.Size = new System.Drawing.Size(35, 20);
 			this.fileToolStripMenuItem.Text = "File";
 			// 
-			// openToolStripMenuItem
-			// 
-			this.openToolStripMenuItem.Name = "openToolStripMenuItem";
-			this.openToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.N)));
-			this.openToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
-			this.openToolStripMenuItem.Text = "new";
-			// 
 			// openToolStripMenuItem1
 			// 
 			this.openToolStripMenuItem1.Name = "openToolStripMenuItem1";
 			this.openToolStripMenuItem1.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
-			this.openToolStripMenuItem1.Size = new System.Drawing.Size(152, 22);
+			this.openToolStripMenuItem1.Size = new System.Drawing.Size(138, 22);
 			this.openToolStripMenuItem1.Text = "open";
 			this.openToolStripMenuItem1.Click += new System.EventHandler(this.openToolStripMenuItem1_Click);
 			// 
@@ -93,14 +86,14 @@
 			// 
 			this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
 			this.saveToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
-			this.saveToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+			this.saveToolStripMenuItem.Size = new System.Drawing.Size(138, 22);
 			this.saveToolStripMenuItem.Text = "save";
 			this.saveToolStripMenuItem.Click += new System.EventHandler(this.save);
 			// 
 			// saveAsToolStripMenuItem
 			// 
 			this.saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
-			this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+			this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(138, 22);
 			this.saveAsToolStripMenuItem.Text = "save as";
 			this.saveAsToolStripMenuItem.Click += new System.EventHandler(this.saveAsToolStripMenuItem_Click);
 			// 
@@ -108,8 +101,9 @@
 			// 
 			this.closeToolStripMenuItem.Name = "closeToolStripMenuItem";
 			this.closeToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.C)));
-			this.closeToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+			this.closeToolStripMenuItem.Size = new System.Drawing.Size(138, 22);
 			this.closeToolStripMenuItem.Text = "close";
+			this.closeToolStripMenuItem.Click += new System.EventHandler(this.closeToolStripMenuItem_Click);
 			// 
 			// openFileDialog
 			// 
@@ -119,14 +113,14 @@
 			// saveFileDialog
 			// 
 			this.saveFileDialog.Filter = "AA2 color files | *.color";
-			this.saveFileDialog.FileOk += new System.ComponentModel.CancelEventHandler(this.saveAs);
+			this.saveFileDialog.FileOk += new System.ComponentModel.CancelEventHandler(this.saveFileDialog_FileOk);
 			// 
 			// panel1
 			// 
 			this.panel1.AutoScroll = true;
 			this.panel1.Location = new System.Drawing.Point(10, 30);
 			this.panel1.Name = "panel1";
-			this.panel1.Size = new System.Drawing.Size(210, 262);
+			this.panel1.Size = new System.Drawing.Size(210, 249);
 			this.panel1.TabIndex = 2;
 			// 
 			// button1
@@ -138,19 +132,20 @@
 			this.button1.Size = new System.Drawing.Size(75, 23);
 			this.button1.TabIndex = 3;
 			this.button1.Text = "Change";
-			this.button1.UseVisualStyleBackColor = true;
+			this.button1.UseVisualStyleBackColor = false;
 			this.button1.Click += new System.EventHandler(this.changeColor);
 			// 
 			// button2
 			// 
-			this.button2.Enabled = false;
 			this.button2.FlatStyle = System.Windows.Forms.FlatStyle.System;
 			this.button2.Location = new System.Drawing.Point(227, 62);
 			this.button2.Name = "button2";
 			this.button2.Size = new System.Drawing.Size(75, 23);
 			this.button2.TabIndex = 4;
+			this.button2.TabStop = false;
 			this.button2.Text = "Add";
-			this.button2.UseVisualStyleBackColor = true;
+			this.button2.UseVisualStyleBackColor = false;
+			this.button2.Click += new System.EventHandler(this.addBox);
 			// 
 			// button3
 			// 
@@ -160,8 +155,31 @@
 			this.button3.Name = "button3";
 			this.button3.Size = new System.Drawing.Size(75, 23);
 			this.button3.TabIndex = 4;
+			this.button3.TabStop = false;
 			this.button3.Text = "Del";
-			this.button3.UseVisualStyleBackColor = true;
+			this.button3.UseVisualStyleBackColor = false;
+			this.button3.Click += new System.EventHandler(this.delBox);
+			// 
+			// textBox1
+			// 
+			this.textBox1.AcceptsReturn = true;
+			this.textBox1.Enabled = false;
+			this.textBox1.Location = new System.Drawing.Point(227, 231);
+			this.textBox1.MaxLength = 3;
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(75, 23);
+			this.textBox1.TabIndex = 5;
+			this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Enabled = false;
+			this.label1.Location = new System.Drawing.Point(226, 213);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(42, 15);
+			this.label1.TabIndex = 6;
+			this.label1.Text = "Alpha";
 			// 
 			// Form1
 			// 
@@ -169,6 +187,8 @@
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.AutoScroll = true;
 			this.ClientSize = new System.Drawing.Size(312, 313);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.textBox1);
 			this.Controls.Add(this.button3);
 			this.Controls.Add(this.button2);
 			this.Controls.Add(this.button1);
@@ -180,6 +200,7 @@
 			this.MinimizeBox = false;
 			this.Name = "Form1";
 			this.Text = "colored";
+			this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
 			this.menuStrip1.ResumeLayout(false);
 			this.menuStrip1.PerformLayout();
 			this.ResumeLayout(false);
@@ -192,8 +213,7 @@
 	public System.Windows.Forms.ColorDialog colorDialog;
         public System.Windows.Forms.Label[] colorBoxes;
         private System.Windows.Forms.MenuStrip menuStrip1;
-        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+	private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem1;
         private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem saveAsToolStripMenuItem;
@@ -204,6 +224,8 @@
 	private System.Windows.Forms.ToolStripMenuItem closeToolStripMenuItem;
 	private System.Windows.Forms.Button button2;
 	private System.Windows.Forms.Button button3;
+	private System.Windows.Forms.TextBox textBox1;
+	private System.Windows.Forms.Label label1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -3,9 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
 
@@ -15,8 +13,8 @@ namespace colored {
 		private int cindex = -1;
 		private bool needSave = false;
 		private string filename;
-		private int last_x;
-		private int last_y;
+		private int nextX;
+		private int nextY;
 
 		public Form1()
 		{
@@ -33,9 +31,9 @@ namespace colored {
 			byte r, g, b;
 
 			try {
-				r = arr[i * 4];
+				b = arr[i * 4];
 			} catch (Exception) {
-				r = 0;
+				b = 0;
 			}
 
 			try {
@@ -45,9 +43,9 @@ namespace colored {
 			}
 
 			try {
-				b = arr[i * 4 + 2];
+				r = arr[i * 4 + 2];
 			} catch (Exception) {
-				b = 0;
+				r = 0;
 			}
 
 			return Color.FromArgb(r, g, b);
@@ -59,6 +57,7 @@ namespace colored {
 			labels[i].SetBounds(x, y, 40, 40);
 			labels[i].BackColor = getColorAt(i, flength, data);
 			labels[i].Click += new EventHandler(labelsOnClick);
+			labels[i].DoubleClick += new EventHandler(labelsDoubleClick);
 			labels[i].Name = i + "";
 			this.panel1.Controls.Add(labels[i]);
 		}
@@ -66,6 +65,7 @@ namespace colored {
 		private void openFile(object sender, CancelEventArgs e)
 		{
 			FileStream file = null;
+			close();
 
 			try {
 				file = File.OpenRead(openFileDialog.FileName);
@@ -80,32 +80,34 @@ namespace colored {
 
 				labels = new List<Label>(n);
 
-				for (int i = 0, y = 0, x = 0; i < n; ) {
+				int i;
+				i = nextX = nextY = 0;
+				while (i < n) {
 					try {
-						createLabel(i, data, x, y, file.Length);
+						createLabel(i, data, nextX, nextY, file.Length);
 						++i;
 
 						if ((i % 4) == 0) {
-							y += 50;
-							x = 0;
+							nextY += 50;
+							nextX = 0;
 						} else {
-							x += 50;
+							nextX += 50;
 						}
 
 					} catch (Exception xx) {
-						MessageBox.Show(xx.ToString());
+						MessageBox.Show(xx.Source + ":\n" + xx.Message);
 						break;
 					}
 				}
 
 				filename = file.Name;
 			} catch (IOException x) {
-				MessageBox.Show(x.ToString());
+				MessageBox.Show(x.Source + ":\n" + x.Message);
 			} catch (Exception x) {
-				MessageBox.Show(x.ToString());
-				if (file != null) {
+				MessageBox.Show(x.Source + ":\n" + x.Message);
+			} finally {
+				if (file != null)
 					file.Dispose();
-				}
 			}
 		}
 
@@ -117,83 +119,217 @@ namespace colored {
 					if (Convert.ToInt32(sn.Name) == cindex)
 						return;
 
-					if (cindex != -1) {
+					if (cindex > -1 && cindex < labels.Count) {
 						labels[cindex].BorderStyle = BorderStyle.None;
+					} else {
+						button3.Enabled = button1.Enabled = true;
 					}
 
 					cindex = Convert.ToInt32(sn.Name);
+					textBox1.Text = labels[cindex].BackColor.A.ToString();
+					label1.Enabled = textBox1.Enabled = true;
 					sn.BorderStyle = BorderStyle.Fixed3D;
 					if (sn.Focused) {
-						sn.Focus();
+						this.panel1.Focus();
 					}
-
-					this.button1.Enabled = true;
 				} catch (Exception ex) {
-					MessageBox.Show(ex.ToString());
+					MessageBox.Show(ex.Source + ":\n" + ex.Message);
 				}
 			}
 		}
 
 		private void changeColor(object sender, EventArgs e)
 		{
-			if (filename != null && cindex != -1) {
+			if (cindex > -1 && cindex < labels.Count) {
+				colorDialog.Color = labels[cindex].BackColor;
 				if (colorDialog.ShowDialog() == DialogResult.OK) {
-					labels[cindex].BackColor = colorDialog.Color;
+					int r = colorDialog.Color.R, g = colorDialog.Color.G, b = colorDialog.Color.B, a = labels[cindex].BackColor.A;
+					labels[cindex].BackColor = Color.FromArgb(a, r, g, b);
 					needSave = true;
 				}
 			}
 		}
 
-		private void save__(string path)
+		private void write(string path)
 		{
-			byte[] data = new byte[labels.Count() * 4];
+			byte[] data = new byte[labels.Count * 4];
+			FileStream file = null;
 
-			for (int i = 0; i < labels.Count(); ++i) {
-				data[i * 4 + 0] = labels[i].BackColor.R;
+			for (int i = 0; i < labels.Count; ++i) {
+				data[i * 4 + 0] = labels[i].BackColor.B;
 				data[i * 4 + 1] = labels[i].BackColor.G;
-				data[i * 4 + 2] = labels[i].BackColor.B;
+				data[i * 4 + 2] = labels[i].BackColor.R;
 				data[i * 4 + 3] = labels[i].BackColor.A;
 			}
 
 			try {
-				FileStream f = File.OpenWrite(path);
-				f.Write(data, 0, data.Length);
-				f.Dispose();
+				file = File.OpenWrite(path);
+				file.Write(data, 0, data.Length);
+				needSave = false;
 			} catch (Exception x) {
-				MessageBox.Show(x.ToString());
+				MessageBox.Show(x.Source + ":\n" + x.Message);
+			} finally {
+				if (file != null)
+					file.Dispose();
 			}
 		}
 
 		private void save(object sender, EventArgs e)
 		{
-			if (needSave && filename != null) {
-				save__(filename);
+			if (needSave) {
+				if (filename == null) {
+					saveFileDialog.ShowDialog();
+				} else {
+					write(filename);
+				}
 			}
-		}
-
-		private void saveAs(object sender, CancelEventArgs e)
-		{
-			if ((saveFileDialog.FileName == filename && !needSave) || filename == null)
-				return;
-			save__(saveFileDialog.FileName);
-			filename = saveFileDialog.FileName;
 		}
 
 		private void saveAsToolStripMenuItem_Click(object sender, EventArgs e)
 		{
 			saveFileDialog.ShowDialog();
 		}
-		
-		private void close__()
+
+		private void close()
 		{
-			for (int i = 0; i < labels.Count(); ++i) {
-				labels[i].Dispose();
+			if (labels != null) {
+				if (needSave) {
+					if (MessageBox.Show(this, "Save changes?", "Save?", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes) {
+						if (filename == null) {
+							saveFileDialog.ShowDialog();
+						} else {
+							write(filename);
+						}
+					}
+				}
+				for (int i = 0; i < labels.Count; ++i) {
+					labels[i].Dispose();
+				}
+				filename = null;
+				labels.Clear();
+				cindex = -1;
+				needSave = button3.Enabled = button1.Enabled = false;
+				nextX = nextY = 0;
 			}
-			filename = null;
-			labels.Clear();
-			labels = null;
-			cindex = -1;
-			needSave = button2.Enabled = button3.Enabled = button1.Enabled = false;
+
+		}
+
+		private void labelsDoubleClick(object Sender, EventArgs e)
+		{
+			changeColor(null, null);
+		}
+
+		private void addBox(object sender, EventArgs e)
+		{
+			if (labels == null)
+				labels = new List<Label>(16);
+
+			int n = labels.Count;
+
+			if (colorDialog.ShowDialog() != DialogResult.OK)
+				return;
+
+			labels.Add(new Label());
+			labels[n].SetBounds(nextX, nextY, 40, 40);
+			labels[n].Click += new EventHandler(labelsOnClick);
+			labels[n].DoubleClick += new EventHandler(labelsDoubleClick);
+			labels[n].Name = n + "";
+			this.panel1.Controls.Add(labels[n]);
+
+			if ((labels.Count % 4) == 0) {
+				nextY += 50;
+				nextX = 0;
+			} else {
+				nextX += 50;
+			}
+
+			needSave = true;
+
+			if (cindex > -1) {
+				for (int i = labels.Count - 1; i > cindex + 1; --i) {
+					labels[i].BackColor = labels[i - 1].BackColor;
+				}
+				labels[cindex + 1].BackColor = colorDialog.Color;
+			} else {
+				labels[n].BackColor = colorDialog.Color;
+			}
+		}
+
+		private void delBox(object sender, EventArgs e)
+		{
+			if (cindex > -1) {
+				for (int i = cindex; i + 1 < labels.Count; ++i) {
+					labels[i].BackColor = labels[i + 1].BackColor;
+				}
+
+				labels[labels.Count - 1].Dispose();
+				labels.RemoveAt(labels.Count - 1);
+
+				if (cindex >= labels.Count) {
+					if (cindex - 1 != -1)
+						labelsOnClick(labels[cindex - 1], null);
+				}
+
+				if (nextX == 0) {
+					nextX = 150;
+					nextY -= 50;
+				} else {
+					nextX -= 50;
+				}
+			}
+
+			if (labels.Count == 0) {
+				cindex = -1;
+				label1.Enabled = textBox1.Enabled = button3.Enabled = button1.Enabled = false;
+				textBox1.Clear();
+			}
+
+			needSave = true;
+		}
+
+		private void saveFileDialog_FileOk(object sender, CancelEventArgs e)
+		{
+			write(saveFileDialog.FileName);
+			filename = saveFileDialog.FileName;
+		}
+
+		private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+		{
+			close();
+		}
+
+		private void closeToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			close();
+		}
+
+		private void textBox1_KeyDown(object sender, KeyEventArgs e)
+		{
+		}
+
+		private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+		{
+			if (!(sender is TextBox)) {
+				return;
+			}
+
+			TextBox tb = (TextBox)sender;
+			if (13 == (int)e.KeyChar) {
+				uint a = UInt32.Parse(tb.Text);
+				if (a > 255) {
+					e.Handled = false;
+				} else {
+					if (cindex > -1 && cindex < labels.Count) {
+						labels[cindex].BackColor = Color.FromArgb((int)a, labels[cindex].BackColor.R, labels[cindex].BackColor.G, labels[cindex].BackColor.B);
+						needSave = true;
+					}
+					e.Handled = true;
+				}
+			} else if (Char.IsDigit(e.KeyChar) || Char.IsControl(e.KeyChar)) {
+				e.Handled = false;
+			} else {
+				e.Handled = true;
+			}
 		}
 	}
 }
